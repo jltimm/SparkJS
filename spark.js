4ea01b12ab078f90eceac7d2ab1e56423944f510
jltimm <jacobltimm@gmail.com>
2018-07-18 20:43:56 -0500

    Renaming index file

--- spark.js
+++ spark.js
@@ -3,14 +3,19 @@ var fs = require('fs');
 /**
  * Creates JSON files with tfidf representation from text files synchronously.
  * @param {string} path The path where the files are
- * @param {boolean} shouldGetSubDirs Flag indicating if sub directories should be accessed
+ * @param {boolean} accessSubDirs Flag indicating if sub directories should be accessed
  */
-module.exports = function createJSONFilesSync(path, shouldGetSubDirs)
+module.exports = function createJSONFilesSync(path, accessSubDirs)
+{
+    var files = retrieveFiles(path);
+
+}
+
+function retrieveFiles(path, accessSubDirs)
 {
     var files = [];
     if (fs.existsSync(path) && fs.lstatSync(path).isDirectory())
     {
         files = fs.readdirSync(path);
     }
-
 }
