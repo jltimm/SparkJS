232a49005bc0e7c6bd4fee60e0bfb3c14754e775
jltimm <jacobltimm@gmail.com>
2018-07-18 20:40:51 -0500

    Beginnings

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function main()
-{
-    console.log("Hello, world!");
-}
+var fs = require('fs');
 
-main();
+/**
+ * Creates JSON files with tfidf representation from text files synchronously.
+ * @param {string} path The path where the files are
+ * @param {boolean} shouldGetSubDirs Flag indicating if sub directories should be accessed
+ */
+module.exports = function createJSONFilesSync(path, shouldGetSubDirs)
+{
+    var files = [];
+    if (fs.existsSync(path) && fs.lstatSync(path).isDirectory())
+    {
+        files = fs.readdirSync(path);
+    }
+
+}
