78259834a4cd846ffbd88ee122671c9c6b6f7c7f
jltimm <jacobltimm@gmail.com>
2018-07-18 21:04:36 -0500

    Added walking through directories

--- spark.js
+++ spark.js
@@ -7,15 +7,34 @@ var fs = require('fs');
  */
 module.exports = function createJSONFilesSync(path, accessSubDirs)
 {
-    var files = retrieveFiles(path);
-
+    var files = retrieveFilesSync(path, accessSubDirs);
+    console.log(files);
 }
 
-function retrieveFiles(path, accessSubDirs)
+/**
+ * Retrieves files in a directory syncrhonoushly
+* @param {string} path The path where the files are
+ * @param {boolean} accessSubDirs Flag indicating if sub directories should be accessed
+ */
+function retrieveFilesSync(path, accessSubDirs)
 {
     var files = [];
     if (fs.existsSync(path) && fs.lstatSync(path).isDirectory())
     {
         files = fs.readdirSync(path);
+        if (accessSubDirs)
+        {
+            var subFiles = []
+            for (file in files)
+            {
+                filePath = path + '/' + files[file];
+                if (fs.existsSync(filePath) && fs.lstatSync(filePath).isDirectory())
+                {
+                    subFiles = retrieveFiles(filePath);
+                }
+            }
+            files = files.concat(subFiles);
+        }
     }
+    return files;
 }
