7c31b953dba30c35118d484ebdf240a2b3371790
jltimm <jacobltimm@gmail.com>
2018-07-19 19:14:03 -0500

    Added filtering of files

--- test/test.js
+++ test/test.js
@@ -3,8 +3,7 @@ var spark = require('../spark.js');
 function runTests()
 {
     var files = spark.createJSONFilesSync('./text_files', true);
-    //console.log(files);
+    console.log(files);
 }
-
 
 runTests();
